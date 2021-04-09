cb5791a9a529ff44656b84e5c26190abd57f84ba
KIRIAKE Masanori <seiken@rd01.net>
2021-04-10 03:25:13 +0900

    Changed: 	* よく考えてみたら、Buttonクラスを拡張する必要はなかった。ということで、そのままButtonクラスを使うことにした。

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,7 @@
 #include "soundOut.hpp"
 #include "utility/Button.h"
 
-class MyBtn : public Button
-{
-public:
-  MyBtn(uint8_t pin, uint8_t invert, uint32_t dbTime);
-};
-MyBtn::MyBtn(uint8_t pin, uint8_t invert, uint32_t dbTime) : Button(pin, invert, dbTime) {}
-
-MyBtn mb(26, true, DEBOUNCE_MS);
+Button CheckPin(26, true, DEBOUNCE_MS);
 
 void setup() {
   M5.begin();
@@ -19,10 +12,10 @@ void setup() {
 }
 
 void loop() {
-  if (mb.isPressed()) {
+  if (CheckPin.isPressed()) {
     SOut.beepOut(1500, 5);
   }
   M5.update();
-  mb.read(); // ボタンのステータスを更新、M5.update()で呼ばれるべき処理
+  CheckPin.read(); // ボタンのステータスを更新、M5.update()で呼ばれるべき処理
   delay(1);
 }
